f2825f2912cb52de5cfde97fca307a02b24faf47
Christian Grobmeier <cg@grobmeier.de>
2014-03-13 14:49:36 +0100

    migrated String utility methods from Yext project

---
+++ src/Opendi/Lang/String.php
@@ -0,0 +1,92 @@
+<?php
+namespace Opendi\Lang;
+
+
+class String {
+
+    /**
+     * @param string $needle
+     * @param array $wordPool
+     */
+    public static function mostSimilar($needle, $wordPool) {
+        $distancePool = [];
+
+        $needle = strtolower($needle);
+
+        foreach ($wordPool as $word) {
+            $distance = levenshtein($needle, strtolower($word));
+
+            if (!isset($distancePool[$distance])) {
+                $distancePool[$distance] = [];
+            }
+
+            $distancePool[$distance][] = $word;
+        }
+
+        $min = min(array_keys($distancePool));
+
+        // if distance is the same, we just pick the first we can get
+        return $distancePool[$min][0];
+    }
+
+    public static function plainPhone($area, $extension) {
+        if ($area == null) {
+            $area = "";
+        }
+        if ($extension == null) {
+            $extension = "";
+        }
+
+        $phoneNumber = trim($area).trim($extension);
+
+        if ($phoneNumber == "") {
+            return null;
+        }
+
+        $phoneNumber = str_replace('-', '', $phoneNumber);
+        return str_replace(' ', '', $phoneNumber);
+    }
+
+    /**
+     * Takes a ten digits string and breaks it into NPA (3 digits),
+     * NXX (3 digits) and the actual phone number (4 digits).
+     *
+     * Format: 234-234-2345
+     *
+     * Returns an array with the keys npa, nxx and num.
+     *
+     * @param $digits
+     */
+    public static function normalizePhone($digits) {
+        $chunks = str_split($digits, 3);
+
+        $result['npa']   = $chunks[0];
+        $result['nxx']   = $chunks[1];
+        $result['num']   = $chunks[2].$chunks[3];
+        $result['seven'] = $chunks[1].'-'.$chunks[2].$chunks[3];
+        $result['plainseven'] = $chunks[1].$chunks[2].$chunks[3];
+        $result['full']  = $chunks[0].'-'.$result['seven'];
+
+        return $result;
+    }
+
+    public static function startsWith($haystack, $needle) {
+        return !strncmp($haystack, $needle, strlen($needle));
+    }
+
+    public static function endsWith($haystack, $needle) {
+        return substr($haystack, -strlen($needle)) == $needle;
+    }
+
+    public static function contains($haystack, $needle) {
+        if (strpos($haystack, $needle) !== false) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function shorten($string, $max = 255) {
+        $string = trim($string);
+        return (mb_strlen($string) > $max) ? mb_strimwidth($string, 0, $max - 3, '...') : $string;
+    }
+} 
---
+++ tests/Opendi/Lang/StringTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace Opendi\Lang;
+
+class StringTest extends \PHPUnit_Framework_TestCase {
+
+    public function testMostSimilar() {
+        $pool = [
+            'New York',
+            'New Hampshire',
+            'San Francisco',
+            'New Yaer',
+            'England1',
+            'Boston',
+            'Baltimore'
+        ];
+
+        $this->assertEquals('New York', String::mostSimilar('New Yark', $pool));
+        $this->assertEquals('New Hampshire', String::mostSimilar('Hampshire', $pool));
+        $this->assertEquals('New Yaer', String::mostSimilar('Yaer', $pool));
+        $this->assertEquals('New Yaer', String::mostSimilar('yaer', $pool));
+        $this->assertEquals('England1', String::mostSimilar('Eng', $pool));
+        $this->assertEquals('Baltimore', String::mostSimilar('bostimore', $pool));
+    }
+
+    public function testStartsWith() {
+        $this->assertTrue(String::startsWith('12356566', '123'));
+        $this->assertFalse(String::startsWith('1212356566', '123'));
+        $this->assertFalse(String::startsWith('# 212356566', '123'));
+        $this->assertFalse(String::startsWith(' 12356566', '123'));
+    }
+
+    public function testEndsWith() {
+        $this->assertTrue(String::endsWith('12356566123', '123'));
+        $this->assertFalse(String::endsWith('121235656623', '123'));
+        $this->assertFalse(String::endsWith('# 21235656612', '123'));
+        $this->assertFalse(String::endsWith(' 1235656613', '123'));
+    }
+
+    public function testContains() {
+        $this->assertTrue(String::contains('12356566', '123'));
+        $this->assertTrue(String::contains('11a12356566', '123'));
+        $this->assertTrue(String::contains('aaa123', '123'));
+        $this->assertTrue(String::contains('- $12356566', '123'));
+        $this->assertFalse(String::contains('121256566', '123'));
+        $this->assertFalse(String::contains('# 212456566', '123'));
+        $this->assertFalse(String::contains(' 1255351652663', '123'));
+    }
+
+    public function testNormalizePhone() {
+        $phone = String::normalizePhone('1234567890');
+
+        $this->assertEquals('123', $phone['npa']);
+        $this->assertEquals('456', $phone['nxx']);
+        $this->assertEquals('7890', $phone['num']);
+        $this->assertEquals('456-7890', $phone['seven']);
+        $this->assertEquals('4567890', $phone['plainseven']);
+        $this->assertEquals('123-456-7890', $phone['full']);
+    }
+
+    public function testPlainPhone() {
+       $this->assertEquals('1234537890', String::plainPhone('123', '453-7890'));
+       $this->assertEquals('1234537890', String::plainPhone('123 ', ' 4537890'));
+       $this->assertEquals('1234537890', String::plainPhone('123', '453 7890'));
+       $this->assertEquals('1234537890', String::plainPhone('123', '453-7890 '));
+       $this->assertEquals('1234537890', String::plainPhone(' 123', ' 453-7890'));
+    }
+
+}
+ 
